7c1f8e7e0d669a5af707fe44a20f6a8fd76145d1
Ümit UĞURLU <umitugurlu02@gmail.com>
2020-11-10 04:11:22 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,19 @@
-function ProgressBar (BaslangicSirasi: number, ledSayisi: number, durum: boolean) {
+//% color="#9C27B0" weight=100 icon="\uf110"
+namespace progressLoadingAnimation{
+
+//% blockId=progressLoadingAnim
+//% block="progressLoadingBar BaslangicSirasi=$BaslangicSira,LedSayisi=$ledSayisi, Durum=$durum"
+//% ledSayisi.defl=3, BaslangicSira.defl=1
+//% durum.defl=true
+//% ledSayisi.min=1, ledSayisi.max=5
+//% BaslangicSira.min=0 BaslangicSira.max=4
+//% color="#FF5733"
+export function ProgressBar (BaslangicSira: number, ledSayisi: number, durum: boolean): void 
+{
     if (durum) {
         for (let satir = 0; satir <= 4; satir++) {
             for (let sutun = 0; sutun <= ledSayisi - 1; sutun++) {
-                led.plot(satir, sutun + BaslangicSirasi)
+                led.plot(satir, sutun + BaslangicSira)
                 for (let index = 0; index <= 50; index++) {
                     led.setBrightness(index * 4 + 55)
                     basic.pause(10)
@@ -10,7 +21,7 @@ function ProgressBar (BaslangicSirasi: number, ledSayisi: number, durum: boolean
                 led.setBrightness(55)
             }
         }
-        for (let index = 0; index <= 4; index++) {
+        for (let index2 = 0; index2 <= 4; index2++) {
             led.enable(false)
             basic.pause(100)
             led.enable(true)
@@ -24,3 +35,4 @@ function ProgressBar (BaslangicSirasi: number, ledSayisi: number, durum: boolean
 basic.forever(function () {
     ProgressBar(1, 2, true)
 })
+}
